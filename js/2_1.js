a3e8cd66768a1ee8412e7d628b0f9f9f640223bb
papanugget <evilgenius04@gmail.com>
2018-02-14 15:43:55 -0500

    finished logical operators / starting switches

--- js/2_1.js
+++ js/2_1.js
@@ -385,3 +385,90 @@
 // console.log(birthday);
 
 //If statements / comparison operators
+// if(something is true){
+//         do something;
+// } else {
+//         do something else
+// }
+
+const id = 100;
+// Equal to
+// if(id == 100){
+//         console.log('Correct');
+// } else {
+//         console.log('Incorrect');
+// }
+
+// //Not equal to
+// if(id != 101){
+//         console.log('Correct');
+// } else {
+//         console.log('Incorrect');
+// }
+
+// // Equal to value & type
+// if(id === 100){
+//         console.log('Correct');
+// } else {
+//         console.log('Incorrect');
+// }
+
+// if(id !== 10){
+//         console.log('Correct');
+// } else {
+//         console.log('Incorrect');
+// }
+// if(typeof id !== 'undefined'){  //checks existence of var
+//         console.log(`The ID is ${id}.`);
+// } else {
+//         console.log('No ID');
+// }
+
+//Greater or less than
+// if(id >= 100){
+//         console.log('Correct');
+// } else {
+//         console.log('Incorrect');
+// }
+
+//If Else
+const color = 'yellow';
+
+// if(color === 'red') {
+//         console.log('Color is red');
+// } else if(color === 'blue') {
+//         console.log('Color is blue');
+// } else {
+//         console.log('Color is not red or blue');
+// }
+
+//Logical operators
+// const name = 'Jorah';
+// const age = '15';
+
+// //&& - And
+// if(age > 0 && age < 12){
+//         console.log(`${name} is a child`);
+// } else if(age >= 13 && age < 19){
+//         console.log(`${name} is a teenager`);
+// } else {
+//         console.log(`${name} is an adult`);
+// }
+
+// // || - Or
+// if(age < 16 || age > 65){
+//         console.log(`${name} cannot run in the race.`);
+// } else {
+//         console.log(`${name} is registered for the race`);
+// }
+
+// // Ternary
+// console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
+
+// //Without braces
+// if(id === 100)
+//         console.log('Correct!');
+// else
+//         console.log('Incorrect!');
+
+//Switches
