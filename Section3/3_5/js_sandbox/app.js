38bff44a5b18e9fbe4fb0ff5ccf243dbfc5ec897
papanugget <evilgenius04@gmail.com>
2018-02-20 16:01:09 -0500

    completed travsersing DOM

--- Section3/3_5/js_sandbox/app.js
+++ Section3/3_5/js_sandbox/app.js
@@ -6,49 +6,48 @@ const listItem = document.querySelector('li.collection-item:first-child');
 val = listItem;
 val = list;
 
-// Get child nodes
-val = list.childNodes;
+//Get child nodes
+val = list.childNodes;  //returns nodeList
 val = list.childNodes[0];
 val = list.childNodes[0].nodeName;
 val = list.childNodes[3].nodeType;
 
 // 1 - Element
-// 2 - Attribute (deprecated)
-// 3 - Text node
+// 2 - Attribute (deprecate)
+// 3 - Text Node
 // 8 - Comment
 // 9 - Document itself
 // 10 - Doctype
 
 
-// Get children element nodes
-val = list.children;
-val = list.children[1];
+//get children element nodes
+val = list.children; //returns HTMLCollection
+val = list.children[0];
 list.children[1].textContent = 'Hello';
-// Children of children
+//children of children
 list.children[3].children[0].id = 'test-link';
 val = list.children[3].children[0];
 
-// First child
-val = list.firstChild;
-val = list.firstElementChild;
-
-// Last child
+//first child
+val = list.firstChild;  //gives text content of firstChild
+val = list.firstElementChild; //give first element not just the textContent
+//last child
 val = list.lastChild;
 val = list.lastElementChild;
 
-// Count child elements
+// count child elements
 val = list.childElementCount;
 
-// Get parent node
+//get parent node
 val = listItem.parentNode;
 val = listItem.parentElement;
 val = listItem.parentElement.parentElement;
 
-// Get next sibling
+//get next sibling
 val = listItem.nextSibling;
-val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
+val = listItem.nextElementSibling;
 
-// Get prev sibling
+//get previous sibling
 val = listItem.previousSibling;
 val = listItem.previousElementSibling;
 console.log(val);
