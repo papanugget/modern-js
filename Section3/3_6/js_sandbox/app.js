e35c538208d18c9d3e8bd447712b6a56704d44f2
papanugget <evilgenius04@gmail.com>
2018-02-21 16:04:58 -0500

    Finished creating elements

---
+++ Section3/3_6/js_sandbox/app.js
@@ -0,0 +1,35 @@
+// Creating Elements
+
+// create element
+const li = document.createElement('li');
+
+//add class
+li.className = 'collection-item';
+
+// add id
+li.id = 'new-item';
+
+// add attribute
+li.setAttribute('title', 'New Item'); 
+
+//Create text node and append
+li.appendChild(document.createTextNode('Hello World'));
+
+// create new link element
+const link = document.createElement('a');
+
+//add classes
+link.className = 'delete-item secondary-content';
+
+//add icon html
+link.innerHTML = '<i class="fa fa-remove"></i>';
+
+//append link icon to to li 
+li.appendChild(link);
+
+// append li as child to ul
+document.querySelector('ul.collection').appendChild(li);
+
+
+
+console.log(li);
