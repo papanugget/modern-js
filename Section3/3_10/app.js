450f6de65350834bbcb3343ea27d5ee59ff2c330
papanugget <evilgenius04@gmail.com>
2018-02-22 15:24:43 -0500

    Finished keyboard events

---
+++ Section3/3_10/app.js
@@ -0,0 +1,39 @@
+// Keyboard and Input Events
+
+const form = document.querySelector('form');
+const taskInput = document.getElementById('task');
+const heading = document.querySelector('h5');
+const select = document.querySelector('select');
+
+//Clears input
+taskInput.value = '';
+
+// form.addEventListener('submit', runEvent);
+
+//keydown
+// taskInput.addEventListener('keydown', runEvent);
+//keyup
+// taskInput.addEventListener('keyup', runEvent);
+//keypress
+// taskInput.addEventListener('keypress', runEvent);
+//focus
+// taskInput.addEventListener('focus', runEvent);
+//blur
+// taskInput.addEventListener('blur', runEvent);
+//cut
+// taskInput.addEventListener('cut', runEvent);
+//paste
+// taskInput.addEventListener('paste', runEvent);
+//input
+// taskInput.addEventListener('input', runEvent);
+//change event -  used for select lists
+select.addEventListener('change', runEvent);
+
+function runEvent(e){
+  console.log(`EVENT TYPE: ${e.type}`);
+  console.log(e.target.value);
+  // heading.innerText = e.target.value;
+  //get input value
+  // console.log(taskInput.value);
+  // e.preventDefault();
+}
