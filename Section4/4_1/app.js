da52e390758830561a601d65a9ae26e84019b16e
papanugget <evilgenius04@gmail.com>
2018-02-26 19:40:34 -0500

    completed task list project with an es6 refactor

--- Section4/4_1/app.js
+++ Section4/4_1/app.js
@@ -5,11 +5,14 @@ const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
+
 //function to load all event listeners
 loadEventListeners();
 
 //functions
 function loadEventListeners(){
+  //DOM load event to show previously entered tasks
+  document.addEventListener('DOMContentLoaded', getTasks);
   // add task event
   form.addEventListener('submit', addTask);
   //Remove task event
@@ -18,6 +21,34 @@ function loadEventListeners(){
   clearBtn.addEventListener('click', clearTasks);
   //Filter tasks event
   filter.addEventListener('keyup', filterTasks);
+}
+// Get Tasks from localstore
+function getTasks(){
+  let tasks; //local scope
+  if(localStorage.getItem('tasks') === null){
+    tasks = []
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  //loop thru existing tasks and create DOM elements
+  tasks.forEach(function(task){
+    //create li element for tasklist
+    const li = document.createElement('li');
+    //add a class
+    li.className = 'collection-item';
+    //create text node and append to li
+    li.appendChild(document.createTextNode(task));
+    //create new link element
+    const link = document.createElement('a');
+    //add a class
+    link.className = 'delete-item secondary-content';
+    //add icon html
+    link.innerHTML = '<i class="fas fa-times"></i>';
+    //append link to li
+    li.appendChild(link);
+    //append li to ul
+    taskList.appendChild(li);
+  });
 }
 // add task
 function addTask(e){
@@ -38,12 +69,30 @@ function addTask(e){
   link.innerHTML = '<i class="fas fa-times"></i>';
   //append link to li
   li.appendChild(link);
-
   //append li to ul
   taskList.appendChild(li);
+  //store in localStorage
+  storeTaskInLocalStorage(taskInput.value);
   //clear input
   taskInput.value = '';
   e.preventDefault();
+}
+
+//localstorage function add local storage for data persistence
+function storeTaskInLocalStorage(task){
+  let tasks;
+  //check local storage and pull data if it exists
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    //gets tasks array if data exists but as a string so need to parse
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  //pushes new task to end of tasks array after checking 
+  tasks.push(task);
+  //reset localStorage array with new value task
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  alert('Task Saved');
 }
 
 //Remove Task
@@ -54,10 +103,31 @@ function removeTask(e){
     //removes e.target's parent parent which should be the li container element
     if(confirm('Are you sure?')){
       e.target.parentElement.parentElement.remove();
+      //remove from localstore
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
     }
   }
 }
-
+//remove from localstore
+function removeTaskFromLocalStorage(taskItem){
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  //loop through array 
+  tasks.forEach(function(task, index){
+    //checks if the DOM element text matches the array content
+    if(taskItem.textContent === task){
+      //splices using the index of the iteration
+      tasks.splice(index, 1);
+    }
+  });
+  //resets localstore tasks with updated content 
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  // console.log(taskItem);
+}
 //Clear Tasks
 function clearTasks(){
   // taskList.innerHTML = '';
@@ -65,8 +135,14 @@ function clearTasks(){
   while(taskList.firstChild){
     taskList.removeChild(taskList.firstChild);
   }
+  //clear from localstore
+  clearTasksFromLocalStorage();
 }
-
+//clear tasks from localstore
+function clearTasksFromLocalStorage(){
+  localStorage.clear();
+}
+//Filter Tasks
 function filterTasks(e){
   const text = e.target.value.toLowerCase();
   //take all list items and runs forEach
