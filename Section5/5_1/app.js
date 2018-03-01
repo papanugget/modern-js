6901cdf07487eff55aa5cd465b430001ec80dc2b
papanugget <evilgenius04@gmail.com>
2018-03-01 10:34:44 -0500

    Built in constructors

--- Section5/5_1/app.js
+++ Section5/5_1/app.js
@@ -19,7 +19,7 @@ function Person(name, lastName, wolf, dob){
   this.birthday = new Date(dob);
   this.calcAge = function(){
     const diff = Date.now() - this.birthday.getTime();  //current dateTime - birthday
-    const ageDate = new Date(diff);
+    const ageDate = new Date(diff); //built in constructor Date takes diff as param
     return Math.abs(ageDate.getUTCFullYear() - 1970);
    }
 } 
@@ -30,4 +30,68 @@ const arya = new Person('Arya', 'Stark', 'Nimeria', '9-9-2003');
 console.log(jon.calcAge());
 console.log(arya.calcAge());
 console.log(this);  //global scope refers to window object
+
+// Built in constructors
+//string
+const name1 = 'Brandon';
+const name2 = new String('Brandon'); //creates a string Object
+
+name2.foo = 'bar';
+console.log(name1);
+console.log(name2);
+
+console.log(typeof name1);  //string
+console.log(typeof name2);  //object
+
+if(name2 === 'Brandon'){
+  console.log('Yes');
+} else {
+  console.log('No');
+}
+
+// number
+const num1 = 5;
+const num2 = new Number(5);
+
+console.log(num2);  //object
+
+// boolean
+const bool1 = true;
+const bool2 = new Boolean(false);
+
+console.log(bool2);
+
+// function
+const getSum1 = function(x, y){
+  return x + y;
+}
+
+const getSum2 = new Function('x', 'y', 'return 1 + 1');
+
+console.log(getSum1(1,1));
+console.log(getSum2());
+
+//objects
+const ned = {
+  name: 'Eddard'
+};
+
+const ned2 = new Object({name: 'Eddard'});
+
+console.log(ned);
+console.log(ned2);
+
+//array
+const arr1 = [1, 2, 3, 4];
+const arr2 = new Array(5, 6, 7, 8);
+
+console.log(arr1);
+console.log(arr2);
+
+//regexp
+const re1 = /\w+/;
+const re2 = new RegExp('\\w+');
+
+console.log(re1);
+console.log(re2);
 
