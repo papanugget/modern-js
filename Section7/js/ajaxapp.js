e40ff97d6ed39a9f4141cf550c4157d108f71008
papanugget <evilgenius04@gmail.com>
2018-03-15 10:14:59 -0400

    Added POST request functionality

--- Section7/js/ajaxapp.js
+++ Section7/js/ajaxapp.js
@@ -4,10 +4,34 @@ console.log('Custom AJAX Library connected');
 const http = new easyHTTP();
 
 //get posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, response){
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, response){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(response);
+//     }
+// });
+
+// get single post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, response){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(response);
+//     }
+// });
+
+//create data
+const data = {
+    title: "Custom Post",
+    body: "This is a custom post for testing"
+};
+
+//post data
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, response){
     if(err){
         console.log(err);
     } else {
-        console.log(response);
+        console.log(response)
     }
 });
