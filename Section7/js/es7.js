616d1836c71b78c07701f9a262b5250e6b912495
papanugget <evilgenius04@gmail.com>
2018-03-16 10:28:13 -0400

    ES7 async await

--- Section7/js/es7.js
+++ Section7/js/es7.js
@@ -1,7 +1,34 @@
 console.log('ES7 Connected');
 
-function myFunc(){
-    return 'Hello';
+// async function myFunc(){
+//     // return 'Hello';
+
+//     const promise = new Promise((resolve, reject) => {
+//         setTimeout(() => resolve('Hello this is resolved'), 1000)
+//     });
+
+//     const error = false;
+//     if(!error) {
+//         const response = await promise;  //wait till promise is reolved
+//         return response;
+//     } else {
+//         await Promise.reject(new Error('something went wrong'));
+//     }
+// }
+
+// // console.log(myFunc());
+
+// myFunc()
+//     .then(response => console.log(response))
+//     .catch(error => console.log(error));
+
+async function getUsers(){
+    //await response from url of fetch call
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    //only proceed once its resolved
+    const data = await response.json();
+    //only proceed once second promise is resolved
+    return data;
 }
 
-console.log(myFunc());
+getUsers().then(users => console.log(users));
