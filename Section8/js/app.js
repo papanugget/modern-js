6aa678538a61c479d3ee4199a8e0787fc10c6a2f
papanugget <evilgenius04@gmail.com>
2018-03-16 12:38:39 -0400

    event listener for search box, github constructor

--- Section8/js/app.js
+++ Section8/js/app.js
@@ -1,1 +1,29 @@
 console.log('Github finder app connected');
+//Init Github constructor
+const github = new GitHub();
+
+//Search Input
+const searchUser = document.getElementById('searchUser');
+
+//event listener for search input
+searchUser.addEventListener('keyup', (e) => {
+    //get input text value
+    const userText = e.target.value;
+    //validate input
+    if(userText != '') {
+        // console.log(userText);
+        //make HTTP call
+        github.getUser(userText)
+            .then(data => {
+                // console.log(data);
+                if(data.profile.message === 'Not Found'){
+                    //show alert user is not found -UI
+
+                } else {
+                    //show profile -UI
+                }
+            })
+    } else {
+        //clear profile -UI
+    }
+});
--- Section8/js/github.js
+++ Section8/js/github.js
@@ -1,1 +1,16 @@
 console.log('github.js  connected');
+
+class GitHub {
+    constructor(){
+        this.client_id = 'bd03f9ee4a38cbaa5d52';
+        this.client_secret = 'a15fc78508af5eec561e9fb4bcdc7983809af40c';
+    }
+    async getUser(user){
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        return {
+            profile
+        }
+    }
+}
