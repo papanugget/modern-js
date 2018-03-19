387e9784e5efc7f878983d9432f3294cb64a6852
papanugget <evilgenius04@gmail.com>
2018-03-19 15:08:34 -0400

    added changelocation functionality and localstorage

--- Section8/weather/js/app.js
+++ Section8/weather/js/app.js
@@ -1,18 +1,36 @@
+//init storage object
+const storage = new Storage();
+//get stored location data
+const weatherLocation = storage.getLocationData();
 //init new weather object
-const weather = new Weather('New York', 'NY');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 //init UI object
 const ui = new UI();
 
 //call getweather function when DOM loads
 document.addEventListener('DOMContentLoaded', getWeather);
 
-// weather.changeLocation('Miami', 'FL');
+//Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+    weather.changeLocation(city, state);
+    //setLocalstorage for new city state
+    storage.setLocationData(city, state);
+    //getWeather after inputting new values
+    getWeather();
+    //close modal
+    $('#locModal').modal('hide');
+});
+
+//weather.changeLocation('Miami', 'FL');
 function getWeather(){
     //returns a promise object
     weather.getWeather()
         .then(results => {
             //insert results into UI
-            console.log(results);
+            // console.log(results);
+            //call UI constructor to insert results into DOM
             ui.paint(results);
         })
         .catch(err => console.log(err));
